98e2bbb89676b35d37ba99b3bd948368e9eab459
Eugenemdk <medyanik.jenya@gmail.com>
2022-09-26 18:04:04 +0300

    Initial commit

---
+++ build/CMakeFiles/IPV4_MAPPEDIPV6.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IPV4_MAPPEDIPV6.dir/main.cpp.o"
+  "CMakeFiles/IPV4_MAPPEDIPV6.dir/main.cpp.o.d"
+  "IPV4_MAPPEDIPV6"
+  "IPV4_MAPPEDIPV6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IPV4_MAPPEDIPV6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
